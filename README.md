cbd30785851c171a6981886b19314f60e90e7124
Ahsan Habib <87078868+carbon-ahs@users.noreply.github.com>
2024-09-07 12:10:21 +0600

    save on shared pref done

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Philip Lakner  
 Link: https://www.youtube.com/watch?v=wtpRp2IpCSo
+KTX: https://www.geeksforgeeks.org/how-to-acess-ui-elements-directly-in-android-using-kotlin-synthetic-property/
+
--- app/src/main/java/com/axiagroups/android_sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/axiagroups/android_sharedpreferences/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.axiagroups.android_sharedpreferences
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.axiagroups.android_sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding //add here_
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val pref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+
+        binding.btnSave.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val age = binding.etAge.text.toString().toInt()
+            val isAdult = binding.cbAdult.isChecked
+
+            editor.apply {
+                putString("name", name)
+                putInt("age", age)
+                putBoolean("isAdult", isAdult)
+                apply()
+            }
+
+        }
+
     }
 }
